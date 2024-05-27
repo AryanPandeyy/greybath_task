478da1ce7ba7a96167767844b5a6f45eb178f0b8
Aryan Pandey <margiaryan@gmail.com>
2024-05-27 21:26:36 +0530

    formatting

--- login.php
+++ login.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(isset($_SESSION["isUser"])) {
+        header("Location: dashboard.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,28 +15,32 @@
 
 <body>
   <?php
-  $username = "";
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["username"]) or empty($_POST["password"])) {
-      echo '<script>alert("All fields are must")</script>';
-    } else {
-      require_once "./db/db.php";
-      $username = htmlspecialchars($_POST["username"]);
-      $isUser = $conn->query("SELECT * FROM users WHERE username = '$username'");
-      if ($isUser->num_rows > 0) {
-        $row = $isUser->fetch_row();
-        $result = password_verify($_POST["password"], $row[3]);
-        if ($result) {
-          echo '<script>window.location.href = "dashboard.php"</script>';
+    $username = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["username"]) or empty($_POST["password"])) {
+            echo '<script>alert("All fields are must")</script>';
         } else {
-          echo "ERROR: $conn->error";
+            include_once "./db/db.php";
+            $username = htmlspecialchars($_POST["username"]);
+            $isUser = $conn->query("SELECT * FROM users WHERE username = '$username'");
+            if ($isUser->num_rows > 0) {
+                $row = $isUser->fetch_row();
+                $result = password_verify($_POST["password"], $row[3]);
+                if ($result) {
+                    session_start();
+                    $_SESSION["isUser"] = true;
+                    $_SESSION["username"] = $username;
+                    header("Location: dashboard.php");
+
+                } else {
+                    echo '<script>alert("Wrong password, please try again with right password")</script>';
+                }
+            } else {
+                echo '<script>alert("Username not found, Please register first")</script>';
+            }
         }
-      } else {
-        echo '<script>alert("Username not found, Please register first")</script>';
-      }
     }
-  }
-  ?>
+    ?>
   <h1>Login</h1>
   <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF"]); ?>" method="post">
     <label for="username">Username:</label>
--- register.php
+++ register.php
@@ -11,27 +11,27 @@
   <?php
   $username = $email = $password = "";
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    require_once "./db/db.php";
-    if (empty($_POST["username"]) or empty($_POST["email"]) or empty($_POST["password"])) {
-      echo '<script>alert("All fields are must")</script>';
-    } else {
-      $username = htmlspecialchars($_POST["username"]);
-      $email = htmlspecialchars($_POST["email"]);
-      $password = password_hash(htmlspecialchars($_POST["password"]), PASSWORD_BCRYPT);
-      $result = $conn->query("SELECT * FROM users WHERE username = '$username' OR email = '$email'");
-      if ($result->num_rows > 0) {
-        echo '<script>alert("Username or Email already exist")</script>';
+      include_once "./db/db.php";
+      if (empty($_POST["username"]) or empty($_POST["email"]) or empty($_POST["password"])) {
+          echo '<script>alert("All fields are must")</script>';
       } else {
-        $sql = "INSERT INTO `users` (`username`, `email`, `password_hash`) VALUES ('" . $username . "', '" . $email . "', '" . $password . "');";
-        if ($conn->query($sql) == true) {
-          echo '<script>window.location.href = "login.php"</script>';
-        } else {
-          echo "ERROR: $conn->error";
-        }
+          $username = htmlspecialchars($_POST["username"]);
+          $email = htmlspecialchars($_POST["email"]);
+          $password = password_hash(htmlspecialchars($_POST["password"]), PASSWORD_BCRYPT);
+          $result = $conn->query("SELECT * FROM users WHERE username = '$username' OR email = '$email'");
+          if ($result->num_rows > 0) {
+              echo '<script>alert("Username or Email already exist")</script>';
+          } else {
+              $sql = "INSERT INTO `users` (`username`, `email`, `password_hash`) VALUES ('" . $username . "', '" . $email . "', '" . $password . "');";
+              if ($conn->query($sql) == true) {
+                  echo '<script>window.location.href = "login.php"</script>';
+              } else {
+                  echo "ERROR: $conn->error";
+              }
+          }
       }
-    }
   }
-  ?>
+    ?>
   <h1>Register</h1>
   <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF"]); ?>" method="post">
     <label for="username">Username:</label>
