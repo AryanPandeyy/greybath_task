96b4df7218fc38ca013ca4ccc2202dd1b6ede293
Aryan Pandey <margiaryan@gmail.com>
2024-06-03 21:20:45 +0530

    typo

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (isset($_SESSION["isUser"])) {
-  header("Location: dashboard.php");
+    header("Location: dashboard.php");
 }
 ?>
 <!DOCTYPE html>
@@ -16,31 +16,31 @@ if (isset($_SESSION["isUser"])) {
 
 <body>
   <?php
-  $username = "";
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["username"]) or empty($_POST["password"])) {
-      echo '<script>alert("All fields are must")</script>';
-    } else {
-      include_once "./db/db.php";
-      $username = htmlspecialchars($_POST["username"]);
-      $isUser = $conn->query("SELECT * FROM users WHERE username = '$username'");
-      if ($isUser->num_rows > 0) {
-        $row = $isUser->fetch_row();
-        $result = password_verify($_POST["password"], $row[3]);
-        if ($result) {
-          session_start();
-          $_SESSION["isUser"] = true;
-          $_SESSION["username"] = $username;
-          header("Location: dashboard.php");
+    $username = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["username"]) or empty($_POST["password"])) {
+            echo '<script>alert("All fields are must")</script>';
         } else {
-          echo '<script>alert("Wrong password, please try again with right password")</script>';
+            include_once "./db/db.php";
+            $username = htmlspecialchars($_POST["username"]);
+            $isUser = $conn->query("SELECT * FROM users WHERE username = '$username'");
+            if ($isUser->num_rows > 0) {
+                $row = $isUser->fetch_row();
+                $result = password_verify($_POST["password"], $row[3]);
+                if ($result) {
+                    session_start();
+                    $_SESSION["isUser"] = true;
+                    $_SESSION["username"] = $username;
+                    header("Location: dashboard.php");
+                } else {
+                    echo '<script>alert("Wrong password, please try again with right password")</script>';
+                }
+            } else {
+                echo '<script>alert("Username not found, Please register first")</script>';
+            }
         }
-      } else {
-        echo '<script>alert("Username not found, Please register first")</script>';
-      }
     }
-  }
-  ?>
+    ?>
   <div class="container">
     <h1>Login</h1>
     <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF"]); ?>" method="post">
@@ -48,7 +48,7 @@ if (isset($_SESSION["isUser"])) {
       <input type="text" name="username" required />
       <label for="password">Password:</label>
       <input type="password" name="password" required />
-      <button type="submit">Register</button>
+      <button type="submit">Login</button>
     </form>
     <p>Dont have an account? <a href="register.php">Register</a></p>
   </div>
