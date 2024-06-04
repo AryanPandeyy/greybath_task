2f1006c4dbc46b01158c510a4d9c92f7ce7f7457
Aryan Pandey <margiaryan@gmail.com>
2024-06-04 14:03:11 +0530

    feat: display products

--- dashboard.php
+++ dashboard.php
@@ -1,20 +1,48 @@
 <?php
 session_start();
 if (!isset($_SESSION["isUser"])) {
-  header("Location: login.php");
+    header("Location: login.php");
+    exit();
 }
 ?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>GreyBath</title>
+  <link rel="stylesheet" href="./style/styles.css">
 </head>
 
 <body>
-  <a href="profile.php">Profile</a>
-  <a href="logout.php">LogOut</a>
+  <div class="container">
+    <?php
+    require_once "./db/db.php";
+    $result = $conn->query(
+        "SELECT `product_name`, `product_desc`, users.username FROM `Products` JOIN `users` ON `users_id` = users.id;"
+    );
+    if ($result) {
+        $products = $result->fetch_all(MYSQLI_ASSOC);
+        foreach ($products as $product) {
+            if (isset($product["product_name"]) && isset($product["product_desc"])) {
+                echo "<div class='product'>";
+                echo "<h2>" . htmlspecialchars($product["product_name"]) . "</h2>";
+                echo "<p>" . htmlspecialchars($product["product_desc"]) . "</p>";
+                echo "<p><strong>Created by: </strong>" . htmlspecialchars($product["username"]) . "</p>";
+                echo "</div><br>";
+            }
+        }
+    } else {
+        echo "Error: " . $conn->error;
+    }
+    ?>
+    <a href="profile.php">Profile</a>
+    <br>
+    <a href="create_product.php">Create Product?</a>
+    <br>
+    <a href="logout.php">LogOut</a>
+  </div>
 </body>
 
 </html>
